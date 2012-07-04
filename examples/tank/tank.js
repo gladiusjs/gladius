ebd62ece4cbe4700b45d0d4a311a69f42e45c399
David Perit <dperit@gmail.com>
2012-07-04 17:13:15 -0400

    Added a rotating turret Added a second tank which is coloured red Added 4 walls around the arena

--- examples/tank/tank.js
+++ examples/tank/tank.js
@@ -45,6 +45,14 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
         '&bumpTexture=../assets/images/tank-bump.jpg' +
         '&normalTexture=../assets/images/tank-normal.jpg';
 
+      var redMaterialArgs = '?colorTexture=../assets/images/red-tank-diffuse.jpg' +
+        '&bumpTexture=../assets/images/tank-bump.jpg' +
+        '&normalTexture=../assets/images/tank-normal.jpg';
+
+      var wallMaterialArgs = '?colorTexture=../assets/images/cube-impulse-diffuse.jpg' +
+        '&bumpTexture=../assets/images/cube-impulse-bump.jpg' +
+        '&normalTexture=../assets/images/cube-impulse-normal.jpg';
+
       engine.get(
         [
           {
@@ -86,13 +94,43 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
             },
             onfailure: function( error ) {
             }
-          },          
+          },
+          {
+            type: cubicvr.Mesh,
+            url: "../assets/procedural-prism.js?length=10&width=1&depth=1",
+            load: engine.loaders.procedural,
+            onsuccess: function( mesh ) {
+              resources.wall = mesh;
+            },
+            onfailure: function( error ) {
+            }
+          },
           {
             type: cubicvr.MaterialDefinition,
             url: "../assets/procedural-material.js" + materialArgs,
             load: engine.loaders.procedural,
             onsuccess: function( material ) {
               resources.material = material;
+            },
+            onfailure: function( error ) {
+            }
+          },
+          {
+            type: cubicvr.MaterialDefinition,
+            url: "../assets/procedural-material.js" + redMaterialArgs,
+            load: engine.loaders.procedural,
+            onsuccess: function( material ) {
+              resources.redMaterial = material;
+            },
+            onfailure: function( error ) {
+            }
+          },
+          {
+            type: cubicvr.MaterialDefinition,
+            url: "../assets/procedural-material.js" + wallMaterialArgs,
+            load: engine.loaders.procedural,
+            onsuccess: function( material ) {
+              resources.wallMaterial = material;
             },
             onfailure: function( error ) {
             }
@@ -121,8 +159,13 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
     var input = engine.findExtension( "gladius-input" );
     var Entity = engine.Entity;
 
+    var tankMovementSpeed = 0.003;
+    var tankRotationSpeed = 0.002;
+    var turretRotationSpeed = 0.002;
+
     var lightDefinition = new cubicvr.LightDefinition({
-      intensity: 2,
+      intensity: 1.5,
+      distance: 30,
       light_type: cubicvr.LightDefinition.LightTypes.POINT,
       method: cubicvr.LightDefinition.LightingMethods.DYNAMIC
     });
@@ -132,9 +175,10 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
         if( this.owner.hasComponent( "Controller" ) ) {
           var controller = this.owner.findComponent( "Controller" );
           var transform = space.findNamed( "tank-body" ).findComponent( "Transform" );
+          var turretTransform = space.findNamed ("tank-turret").findComponent( "Transform" );
           if( controller.states["MoveForward"] ) {
             console.log( this.owner.id, "Move forward!" );
-            var direction = math.transform.translate( [space.clock.delta * 0.001, 0, 0] );
+            var direction = math.transform.translate( [space.clock.delta * tankMovementSpeed, 0, 0] );
             var rotation = math.transform.rotate( transform.rotation );
             direction = math.matrix4.multiply( [direction, rotation] );
             direction = [direction[12], direction[13], direction[14]];
@@ -142,7 +186,7 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
           }
           if( controller.states["MoveBackward"] ) {
             console.log( this.owner.id, "Move backward!" );            
-            var direction = math.transform.translate( [space.clock.delta * -0.001, 0, 0] );
+            var direction = math.transform.translate( [space.clock.delta * -tankMovementSpeed, 0, 0] );
             var rotation = math.transform.rotate( transform.rotation );
             direction = math.matrix4.multiply( [direction, rotation] );
             direction = [direction[12], direction[13], direction[14]];
@@ -151,7 +195,7 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
           if( controller.states["TurnLeft"] ) {
             if( controller.states["StrafeModifier"] ) {
               console.log( this.owner.id, "Strafe left!" );
-              var direction = math.transform.translate( [0, space.clock.delta * -0.001, 0] );
+              var direction = math.transform.translate( [0, space.clock.delta * -tankMovementSpeed, 0] );
               var rotation = math.transform.rotate( transform.rotation );
               direction = math.matrix4.multiply( [direction, rotation] );
               direction = [direction[12], direction[13], direction[14]];
@@ -159,13 +203,13 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
             } else {
               console.log( this.owner.id, "Turn left!" );
               var rotation = transform.rotation;
-              transform.setRotation( math.vector3.add( rotation, [0, 0, space.clock.delta * -0.001] ) );
+              transform.setRotation( math.vector3.add( rotation, [0, 0, space.clock.delta * -tankRotationSpeed] ) );
             }
           }
           if( controller.states["TurnRight"] ) {
             if( controller.states["StrafeModifier"] ) {
               console.log( this.owner.id, "Strafe right!" );
-              var direction = math.transform.translate( [0, space.clock.delta * 0.001, 0] );
+              var direction = math.transform.translate( [0, space.clock.delta * tankMovementSpeed, 0] );
               var rotation = math.transform.rotate( transform.rotation );
               direction = math.matrix4.multiply( [direction, rotation] );
               direction = [direction[12], direction[13], direction[14]];
@@ -173,8 +217,18 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
             } else {
               console.log( this.owner.id, "Turn right!" );
               var rotation = transform.rotation;
-              transform.setRotation( math.vector3.add( rotation, [0, 0, space.clock.delta * 0.001] ) );
+              transform.setRotation( math.vector3.add( rotation, [0, 0, space.clock.delta * tankRotationSpeed] ) );
             }
+          }
+          if (controller.states["TurnTurretLeft"] ) {
+            console.log( this.owner.id, "Turret turn left!" );
+            var rotation = turretTransform.rotation;
+            turretTransform.setRotation( math.vector3.add( rotation, [0, 0, space.clock.delta * -turretRotationSpeed] ) );
+          }
+          if (controller.states["TurnTurretRight"] ) {
+            console.log( this.owner.id, "Turret turn right!" );
+            var rotation = turretTransform.rotation;
+            turretTransform.setRotation( math.vector3.add( rotation, [0, 0, space.clock.delta * turretRotationSpeed] ) );
           }
         }
       },
@@ -183,64 +237,96 @@ document.addEventListener( "DOMContentLoaded", function( e ) {
       }
     };
 
-    // This parent entity will let us adjust the position and orientation of the
-    // tank, and handle game logic events
-    space.add( new Entity( "tank", 
+    function createTank(name, position, material, hasControls) {
+// This parent entity will let us adjust the position and orientation of the
+      // tank, and handle game logic events
+      space.add(new Entity(name,
+        [
+          new engine.core.Transform(position, [math.TAU / 4, 0, 0], [0.5, 0.5, 0.5]),
+          new input.Controller(resources.tankControls),
+          new engine.logic.Actor(tankLogic)
+        ],
+        [name]
+      ));
+      if (hasControls){
+        space.findNamed(name).addComponent(new input.Controller(resources.tankControls));
+      }
+      space.add(new Entity(name + "-body",
+        [
+          new engine.core.Transform(),
+          new cubicvr.Model(resources.tankBody, material)
+        ],
+        [name],
+        space.findNamed(name)
+      ));
+      space.add(new Entity(name + "-tread",
+        [
+          new engine.core.Transform([0, 0.8, 0]),
+          new cubicvr.Model(resources.tankTread, material)
+        ],
+        [name],
+        space.findNamed(name + "-body")
+      ));
+      space.add(new Entity(name + "-tread",
+        [
+          new engine.core.Transform([0, -0.8, 0]),
+          new cubicvr.Model(resources.tankTread, material)
+        ],
+        [name],
+        space.findNamed(name + "-body")
+      ));
+      space.add(new Entity(name+"-turret",
+        [
+          new engine.core.Transform([-0.2, 0, -0.6]),
+          new cubicvr.Model(resources.tankTurret, material)
+        ],
+        [name],
+        space.findNamed(name + "-body")
+      ));
+      space.add(new Entity(name + "-barrel",
+        [
+          new engine.core.Transform([0.8, 0, 0]),
+          new cubicvr.Model(resources.tankBarrel, material)
+        ],
+        [name],
+        space.findNamed(name + "-turret")
+      ));
+    }
+    createTank("tank", [-4,0,-4], resources.material, true);
+    createTank("red-tank", [4,0,4], resources.redMaterial, false);
+
+    //TODO: Make these walls have tiling textures.
+    // TODO: Add in physics bounding boxes for them
+    space.add( new Entity( "wallLeft",
       [
-        new engine.core.Transform( [0, 0, 5], [math.TAU/6, 0, 0], [0.5, 0.5, 0.5] ),
-        new input.Controller( resources.tankControls ),
-        new engine.logic.Actor( tankLogic )
-      ],
-      ["tank"]
+        new engine.core.Transform([-5,0,0], [0,math.TAU/4,0]),
+        new cubicvr.Model(resources.wall, resources.wallMaterial)
+      ]
     ));
-    space.add( new Entity( "tank-body",
+    space.add( new Entity( "wallRight",
       [
-        new engine.core.Transform(),
-        new cubicvr.Model( resources.tankBody, resources.material )
-      ],
-      ["tank"],
-      space.findNamed( "tank" )
+        new engine.core.Transform([5,0,0], [0,math.TAU/4,0]),
+        new cubicvr.Model(resources.wall, resources.wallMaterial)
+      ]
     ));
-    space.add( new Entity( "tank-tread",
+    space.add( new Entity( "wallTop",
       [
-        new engine.core.Transform( [0, 0.8, 0] ),
-        new cubicvr.Model( resources.tankTread, resources.material )
-      ],
-      ["tank"],
-      space.findNamed( "tank-body" )
+        new engine.core.Transform([0,0,-5], [0,0,0]),
+        new cubicvr.Model(resources.wall, resources.wallMaterial)
+      ]
     ));
-    space.add( new Entity( "tank-tread",
+    space.add( new Entity( "wallBottom",
       [
-        new engine.core.Transform( [0, -0.8, 0] ),
-        new cubicvr.Model( resources.tankTread, resources.material )
-      ],
-      ["tank"],
-      space.findNamed( "tank-body" )
-    ));
-    space.add( new Entity( "tank-turret",
-      [
-        new engine.core.Transform( [-0.2, 0, -0.6] ),
-        new cubicvr.Model( resources.tankTurret, resources.material )
-      ],
-      ["tank"],
-      space.findNamed( "tank-body" )
-    ));
-    space.add( new Entity( "tank-barrel",
-      [
-        new engine.core.Transform( [0.8, 0, 0] ),
-        new cubicvr.Model( resources.tankBarrel, resources.material )
-      ],
-      ["tank"],
-      space.findNamed( "tank-turret" )
+        new engine.core.Transform([0,0,5], [0,0,0]),
+        new cubicvr.Model(resources.wall, resources.wallMaterial)
+      ]
     ));
 
     space.add( new Entity( "camera",
       [
-        new engine.core.Transform( [0, 0, 0] ),
-        new cubicvr.Camera({
-          targeted: true
-        }),
-        new cubicvr.Light()
+        new engine.core.Transform( [0, 10, 0], [-math.TAU/4, 0, 0] ),
+        new cubicvr.Camera(),
+        new cubicvr.Light(lightDefinition)
       ]
     ));
     // space.findNamed( "camera" ).findComponent( "Camera" ).setTarget( 0, 0, 0 );
